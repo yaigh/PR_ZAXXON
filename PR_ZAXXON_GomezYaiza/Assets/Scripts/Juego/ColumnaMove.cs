43938646a9671bae640fdd2b1c9d0854b87b5dd3
Yaiza <yaizagh2001@gmail.com>
2021-10-08 17:12:37 +0200

    4.Destruir Obstaculos
    Destruir obstaculos y poner valor a la Y del instanciador

--- PR_ZAXXON_GomezYaiza/Assets/Scripts/Juego/ColumnaMove.cs
+++ PR_ZAXXON_GomezYaiza/Assets/Scripts/Juego/ColumnaMove.cs
@@ -3,24 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ColumnaMuve : MonoBehaviour
+public class ColumnaMove : MonoBehaviour
 {
     float speed;
-   
-    
+  
+
     // Start is called before the first frame update
     void Start()
     {
-        speed = 10f;
-        
+        speed = 20f;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+      
         transform.Translate(Vector3.back * Time.deltaTime * speed);
 
+        float posZ = transform.position.z;
+        if (posZ < -20)
+        {
+            Destroy(gameObject);
+        }
     }
 
-  
+    
+
 }
--- PR_ZAXXON_GomezYaiza/Assets/Scripts/Juego/Instanciador.cs
+++ PR_ZAXXON_GomezYaiza/Assets/Scripts/Juego/Instanciador.cs
@@ -25,6 +25,7 @@ public class Instanciador : MonoBehaviour
         Vector3 destPos = instanciarPos.position;
         Vector3 despl = new Vector3(desplX, 0, 0);
 
+        //transform.position = new Vector3(0f, 0f, 0f);
 
         /*for (int n= 0; n < 5; n++)
         {
@@ -45,10 +46,14 @@ public class Instanciador : MonoBehaviour
     {
         while (true)
         {
-            float randomX = Random.Range(-10f, 10f);
+            float randomX = Random.Range(-20f, 20f);
             Vector3 newPos = new Vector3(randomX, instanciarPos.position.y, instanciarPos.position.z);
             Instantiate(obstaculo, newPos, Quaternion.identity);
 
+            float randomY = Random.Range(-10f, 10f);
+            Vector3 newPosY = new Vector3(instanciarPos.position.x, randomY, instanciarPos.position.z);
+            Instantiate(obstaculo, newPosY, Quaternion.identity);
+           
             yield return new WaitForSeconds(intervalo);
         }
     }
